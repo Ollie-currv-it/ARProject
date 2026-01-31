b36ec5df2ab1ee19bbae19cae9ef25614f0d8d11
revilo llien <oliver.neill@outlook.com>
2026-01-31 16:02:08 +0000

    starting to build view for viewing individual models

--- ShopFront/shopFrontApp/uploadModel/urls.py
+++ ShopFront/shopFrontApp/uploadModel/urls.py
@@ -5,4 +5,5 @@ from uploadModel import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('upload_model/', views.upload_model, name='upload_model'),
+    path('view_model/<int:model_id>/', views.view_model, name='view_model'),
 ]
